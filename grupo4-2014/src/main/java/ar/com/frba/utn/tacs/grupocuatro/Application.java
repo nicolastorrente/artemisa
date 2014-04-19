ee272d04672365f38f8d4f8d05a3d0321c496198
Omar Bascuas <maildeomar@gmail.com>
2014-04-19 18:32:56 -0300

    Cambios para transformar la aplicacin en una webapp, pagina de error generica para capturar todas las excepciones y mostrar un mensaje adecuado al usuario

--- grupo4-2014/src/main/java/ar/com/frba/utn/tacs/grupocuatro/Application.java
+++ grupo4-2014/src/main/java/ar/com/frba/utn/tacs/grupocuatro/Application.java
@@ -2,12 +2,23 @@ package ar.com.frba.utn.tacs.grupocuatro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@ComponentScan
+@Configuration
 @EnableAutoConfiguration
-public class Application {
+@EnableScheduling
+@ComponentScan
+public class Application extends SpringBootServletInitializer{
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+    }
+	
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- grupo4-2014/src/main/java/ar/com/frba/utn/tacs/grupocuatro/controller/RootController.java
+++ grupo4-2014/src/main/java/ar/com/frba/utn/tacs/grupocuatro/controller/RootController.java
@@ -1,8 +1,5 @@
 package ar.com.frba.utn.tacs.grupocuatro.controller;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +9,6 @@ public class RootController {
 
 	@RequestMapping("/")
     public String index(Model model) {
-		Date date = new Date();
-		SimpleDateFormat formatter = new SimpleDateFormat("HH:mm dd-MM-yyyy");
-        model.addAttribute("date", formatter.format(date));
         return "index";
     }
 }
